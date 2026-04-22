82a396f24930aff9f7294bc89d489de6f375d853
Aziel Zhero <aziel.outerheaven25@gmail.com>
2026-04-22 18:22:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6487128292-d2ca9",
+  "appId": "1:778264572179:web:0ad2860547311181ae2ae6",
+  "apiKey": "AIzaSyCI95YXf3wn495X_ZVq01EGg6KWq_VE1PU",
+  "authDomain": "studio-6487128292-d2ca9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "778264572179"
+};
